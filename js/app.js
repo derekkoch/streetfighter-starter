53d674b4695ba4f5fed425923a4d99c8398bb597
derekkoch <derek@freeyourdata.net>
2014-10-06 19:48:39 -0400

    Initial commit after first few hours of development
    Initial commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	});
+
+	$('.ryu').mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	});
+	
+	$('.ryu').mousedown(function() {
+		/*playHadouken();*/
+		$('.ryu-ready').hide();
+		$('.ryu-still').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+	      {'left': '300px'},
+	      500,
+	      function() {
+	        $(this).hide();
+	        $(this).css('left', '-212px');
+	      }
+	    );
+	});
+
+	$('.ryu').mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+
+	$('ryu').keydown(function() {
+
+	});
+
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
